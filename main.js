89a350ff6c564a6ebf692e656f3370abf049db1d
Adam Garwacki <adam.garwacki@gmail.com>
2020-10-18 12:55:07 +0200

    logika ruchu, zmiany w indeksowaniu kafelków

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 // PODAJ LICZBĘ RZĘDÓW I KOLUMN:
-let cols = 8;
-let rows = 7;
+let cols = 5;
+let rows = 5;
 
 // ------------------------------------
 
@@ -13,9 +13,83 @@ gameContainer.style.width = `${containerWidth}px`;
 let containerHeight = rows*100;
 gameContainer.style.height = `${containerHeight}px`;
 
-for (let i = 0; i < cols*rows; i++) {
-    let singleGamePanel = document.createElement('div');
-    singleGamePanel.classList.add('single-panel');
-    gameContainer.appendChild(singleGamePanel);
-    console.log(i);
+
+let gamePanelsArray = [];
+let indexIterator = 0;
+
+for (let i = 0; i < rows; i++) {
+    let arrayRow = [];
+    for (let j = 0; j < cols; j++) {
+        let singleGamePanel = document.createElement('div');
+        singleGamePanel.classList.add('single-panel');
+        singleGamePanel.id = `element-index-${indexIterator}`;
+        gameContainer.appendChild(singleGamePanel);
+        arrayRow.push(singleGamePanel);
+        indexIterator++;
+    }
+    gamePanelsArray.push(arrayRow);
 }
+
+console.log(gamePanelsArray);
+
+// GAME ENGINE
+
+let playerPos = [2, 2];
+gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'yellow';
+
+
+
+
+let isWin = false;
+let pressedKey = 0
+
+document.addEventListener('keydown', (e) => {
+    if (isWin == false) {
+        pressedKey = e.code;
+
+        switch (pressedKey) {
+            case 'ArrowLeft':
+                if (playerPos[1] != 0) {
+                    gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'green';
+                    playerPos[1]--;
+                    break;
+                } else {
+                    break;
+                }
+                
+            case 'ArrowUp':
+                if (playerPos[0] != 0) {
+                    gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'green';
+                    playerPos[0]--;
+                    break;
+                } else {
+                    break;
+                }
+                
+            case 'ArrowRight':
+                if (playerPos[1] != cols-1) {
+                    gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'green';
+                    playerPos[1]++;
+                    break;
+                } else {
+                    break;
+                }
+                
+            case 'ArrowDown':
+                if (playerPos[0] != rows-1) {
+                    gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'green';
+                    playerPos[0]++;
+                    break;
+                } else {
+                    break;
+                }
+        
+            default:
+                break;
+        }
+        
+        gamePanelsArray[playerPos[0]][playerPos[1]].style.backgroundColor = 'yellow';
+    } else {
+        console.log('Wygrałeś!');
+    }
+});
