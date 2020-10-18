9c2772a72743017e797214db8ccf3a914e43e7f0
Adam Garwacki <adam.garwacki@gmail.com>
2020-10-18 12:07:16 +0200

    poprawka do poprzedniego commita (sorry)

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('dzyń');
+'use strict';
+// PODAJ LICZBĘ RZĘDÓW I KOLUMN:
+let cols = 8;
+let rows = 7;
+
+// ------------------------------------
+
+let gameContainer = document.getElementById('game-container');
+
+let containerWidth = cols*100;
+gameContainer.style.width = `${containerWidth}px`;
+
+let containerHeight = rows*100;
+gameContainer.style.height = `${containerHeight}px`;
+
+for (let i = 0; i < cols*rows; i++) {
+    let singleGamePanel = document.createElement('div');
+    singleGamePanel.classList.add('single-panel');
+    gameContainer.appendChild(singleGamePanel);
+    console.log(i);
+}
