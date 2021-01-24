eeb10472546764ca58b192dcd40b08fda49f0d2c
Adam Garwacki <adam.garwacki@gmail.com>
2021-01-24 11:18:13 +0100

    uporządkowanie main.js; generowanie gry całkowicie za pomocą js; generowanie gry teraz w funkcji;

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 'use strict';
 
-// PARAMETRY GRY:
+// TODO:
+// - lepsza funkcja-bezpiecznik (line 66)
+// 
 
+
+// "INIT":
+let gameContainer = document.getElementById('game-container');
+
+// PRESETY MAP:
+let map1 = {
+    'size': 6,
+    'objectiveCount': 5,
+    'obstacleMap': [[0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 1, 0, 1],
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 1, 0]],
+    'playerPosition': [0, 0]
+}
+
+
+// PARAMETRY GRY:
 // NIE USUWAĆ!!!! MOŻE BYĆ POTRZEBNE DO ODKRĘCENIA JAKIEGOŚ BŁĘDU!
 // let mapObject.objectiveCount = 3;
 // let mapObject.obstacleMap = [[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]];
@@ -10,104 +31,204 @@
 // let mapObject.size = 5;
 
 
-let mapObject = {
-    'size': 6,
-    'objectiveCount': 5,
-    'obstacleMap': [[0, 0, 0, 0, 0, 0], 
-                    [0, 1, 0, 1, 0, 1], 
-                    [0, 1, 0, 0, 0, 0], 
-                    [0, 1, 1, 0, 1, 0], 
-                    [0, 0, 0, 0, 0, 0]],
-    'playerPosition': [0, 0]
-}
+// START GRY, GENEROWANIE MAPY, PORUSZANIE SIĘ ETC:
 
-let panelSizePreset = '';
+let startGame = (mapObject) => {
+    let panelSizePreset = '';
+    switch (mapObject.size) {
+        case 2:
+            panelSizePreset = 'single-panel-2';
+            break;
 
-switch (mapObject.size) {
-    case 2:
-        panelSizePreset = 'single-panel-2';
-        break;
+        case 3:
+            panelSizePreset = 'single-panel-3';
+            break;
 
-    case 3:
-        panelSizePreset = 'single-panel-3';
-        break;
+        case 4:
+            panelSizePreset = 'single-panel-4';
+            break;
 
-    case 4:
-        panelSizePreset = 'single-panel-4';
-        break;
+        case 5:
+            panelSizePreset = 'single-panel-5';
+            break;
 
-    case 5:
-        panelSizePreset = 'single-panel-5';
-        break;
-    
-    case 6:
-        panelSizePreset = 'single-panel-6';
-        break;
+        case 6:
+            panelSizePreset = 'single-panel-6';
+            break;
 
-    case 8:
-        panelSizePreset = 'single-panel-8';
-        break;
+        case 8:
+            panelSizePreset = 'single-panel-8';
+            break;
 
-    default:
-        console.log('Nie istnieje preset kafelka dla podanego rozmiaru planszy!');
-        break;
-}
-
-
-// ALERT NA WYPADEK NIEWŁAŚCIWEJ ILOŚCI KOLUMN:
-
-if (((mapObject.size * mapObject.size) - 1) < mapObject.objectiveCount) {
-    alert('UWAGA! ZA MAŁO PÓL - WEJDZIE NIESKOŃCZONA PĘTLA!!!!!!!');
-    mapObject.objectiveCount = 0
-}
-
-
-// ------------------------------------
-
-let gameContainer = document.getElementById('game-container');
-
-// let containerWidth = mapObject.size * 100;
-// gameContainer.style.width = `${containerWidth}px`;
-// let containerHeight = mapObject.size * 100;
-// gameContainer.style.height = `${containerHeight}px`;
-
-let gamePanelsArray = [];
-let indexIterator = 0;
-
-for (let i = 0; i < mapObject.size; i++) {
-    let arrayRow = [];
-    for (let j = 0; j < mapObject.size; j++) {
-        let singlePanelObject = {}
-        let singleGamePanel = document.createElement('div');
-        singleGamePanel.classList.add('single-panel', panelSizePreset);
-
-        // Nadawanie id poszczególnym polom - może się przydać kiedyś!!!
-        // singleGamePanel.id = `element-index-${indexIterator}`;
-
-
-        singlePanelObject.panelId = singleGamePanel;
-        singlePanelObject.isObjective = false;
-        singlePanelObject.isObstacle = false;
-
-
-        gameContainer.appendChild(singleGamePanel);
-
-        arrayRow.push(singlePanelObject);
-        indexIterator++;
+        default:
+            console.log('Nie istnieje preset kafelka dla podanego rozmiaru planszy!');
+            break;
     }
-    gamePanelsArray.push(arrayRow);
-}
-console.log(gamePanelsArray);
 
-// USTAWIANIE MAPY
-mapObject.obstacleMap.forEach(row => {
-    for (let i = 0; i < row.length; i++) {
-        if (row[i] == 1) {
-            gamePanelsArray[mapObject.obstacleMap.indexOf(row)][i].isObstacle = true;
-            gamePanelsArray[mapObject.obstacleMap.indexOf(row)][i].panelId.style.backgroundColor = 'gray';
+    // ALERT NA WYPADEK NIEWŁAŚCIWEJ ILOŚCI KOLUMN: ---- DO POPRAWY - WZIĄĆ POD UWAGĘ ILOŚĆ PRZESZKÓD NA PLANSZY!!!!
+    if (((mapObject.size * mapObject.size) - 1) < mapObject.objectiveCount) {
+        alert('UWAGA! ZA MAŁO PÓL - WEJDZIE NIESKOŃCZONA PĘTLA!!!!!!!');
+        mapObject.objectiveCount = 0
+    }
+    
+    let indexIterator = 0;
+    let gamePanelsArray = [];
+    
+    for (let i = 0; i < mapObject.size; i++) {
+        let arrayRow = [];
+        for (let j = 0; j < mapObject.size; j++) {
+            let singlePanelObject = {}
+            let singleGamePanel = document.createElement('div');
+            singleGamePanel.classList.add('single-panel', panelSizePreset);
+    
+            // Nadawanie id poszczególnym polom - może się przydać kiedyś!!!
+            // singleGamePanel.id = `element-index-${indexIterator}`;
+    
+    
+            singlePanelObject.panelId = singleGamePanel;
+            singlePanelObject.isObjective = false;
+            singlePanelObject.isObstacle = false;
+    
+    
+            gameContainer.appendChild(singleGamePanel);
+    
+            arrayRow.push(singlePanelObject);
+            indexIterator++;
+        }
+        gamePanelsArray.push(arrayRow);
+    }
+    console.log(gamePanelsArray);
+
+    // USTAWIANIE MAPY
+    mapObject.obstacleMap.forEach(row => {
+        for (let i = 0; i < row.length; i++) {
+            if (row[i] == 1) {
+                gamePanelsArray[mapObject.obstacleMap.indexOf(row)][i].isObstacle = true;
+                gamePanelsArray[mapObject.obstacleMap.indexOf(row)][i].panelId.style.backgroundColor = 'gray';
+            }
+        }
+    });
+
+    
+    // USTAWIANIE OBJECTIVE
+    let objectiveCount = mapObject.objectiveCount;
+    let victoryCount = mapObject.objectiveCount;
+
+    let ob = mapObject.objectiveCount;
+    while (ob > 0) {
+        let posX = Math.floor(Math.random() * mapObject.size);
+        let posY = Math.floor(Math.random() * mapObject.size);
+    
+        if ((posX != 0 || posY != 0) && !gamePanelsArray[posY][posX].isObjective && !gamePanelsArray[posY][posX].isObstacle) {
+            gamePanelsArray[posY][posX].isObjective = true;
+            gamePanelsArray[posY][posX].panelId.style.backgroundColor = 'red';
+            ob--;
         }
     }
-})
+
+    let changeObjective = (maxScore, victoryCount) => {
+        document.getElementById('score').innerText = `${maxScore - victoryCount} / ${maxScore}`;
+    }
+    
+    changeObjective(objectiveCount, victoryCount);
+    
+
+    // GAME ENGINE
+    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'gold';
+
+    let playerMovement = (direction) => {
+        switch (direction) {
+            case 'ArrowLeft':
+                if (mapObject.playerPosition[1] != 0 && !gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1] - 1].isObstacle) {
+                    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
+    
+                    mapObject.playerPosition[1]--;
+                    break;
+                } else {
+                    break;
+                }
+    
+            case 'ArrowUp':
+                if (mapObject.playerPosition[0] != 0 && !gamePanelsArray[mapObject.playerPosition[0] - 1][mapObject.playerPosition[1]].isObstacle) {
+                    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
+    
+                    mapObject.playerPosition[0]--;
+                    break;
+                } else {
+                    break;
+                }
+    
+            case 'ArrowRight':
+                if (mapObject.playerPosition[1] != mapObject.size - 1 && !gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1] + 1].isObstacle) {
+                    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
+    
+                    mapObject.playerPosition[1]++;
+                    break;
+                } else {
+                    break;
+                }
+    
+            case 'ArrowDown':
+                if (mapObject.playerPosition[0] != mapObject.size - 1 && !gamePanelsArray[mapObject.playerPosition[0] + 1][mapObject.playerPosition[1]].isObstacle) {
+                    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
+    
+                    mapObject.playerPosition[0]++;
+                    break;
+                } else {
+                    break;
+                }
+    
+            default:
+                break;
+        }
+        gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'gold';
+    
+        if (gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].isObjective == true) {
+            gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].isObjective = false;
+            victoryCount--;
+            changeObjective(objectiveCount, victoryCount);
+        }
+    
+        // Poniższy setTimeout() "opóźnia" (chyba...?) w jakiś sposób wykonanie tego alertu, tak żeby najpierw poszedł DOM
+        // zmieniający kolor kafelka na żółty.
+        // Wiem że działa, ale nie wiem dlaczego.
+    
+        setTimeout(() => {
+            if (victoryCount == 0) {
+                let gameContainerWin = document.createElement('div');
+                gameContainerWin.classList.add('game-container-win');
+                gameContainerWin.id = 'game-container-win';
+    
+                let winMessage = document.createElement('p');
+                winMessage.classList.add('win-message');
+                winMessage.textContent = 'You win!';
+    
+                gameContainerWin.appendChild(winMessage);
+                gameContainer.appendChild(gameContainerWin);
+    
+                // timeout powoduje że widoczna jest animacja - chodzi o coś z renderowaniem elementów html...?
+                setTimeout(() => {
+                    gameContainerWin.style.opacity = 1;
+                }, 10);
+            }
+        }, 0);
+    }
+    
+    
+    // EVENTS HANDLING
+    
+    document.addEventListener('keydown', (e) => {
+        playerMovement(e.code);
+    });
+}
+
+startGame(map1);
+
+
+
+
+
+
 
 
 // GENEROWANIE LABIRYNTU: (NIE DZIAŁA - ZOSTAWIAM JAKO "PAMIĄTKA")
@@ -169,102 +290,3 @@ mapObject.obstacleMap.forEach(row => {
         }
     })
 }) */
-
-
-// USTAWIANIE OBJECTIVE
-let victoryCount = mapObject.objectiveCount;
-
-let changeObjective = () => {
-    document.getElementById('score').innerText = `${mapObject.objectiveCount - victoryCount} / ${mapObject.objectiveCount}`;
-}
-
-let ob = mapObject.objectiveCount;
-while (ob > 0) {
-    let posX = Math.floor(Math.random() * mapObject.size);
-    let posY = Math.floor(Math.random() * mapObject.size);
-
-    if ((posX != 0 || posY != 0) && !gamePanelsArray[posY][posX].isObjective && !gamePanelsArray[posY][posX].isObstacle) {
-        gamePanelsArray[posY][posX].isObjective = true;
-        gamePanelsArray[posY][posX].panelId.style.backgroundColor = 'red';
-        ob--;
-    }
-}
-
-changeObjective();
-
-
-
-// GAME ENGINE
-
-gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'gold';
-
-let playerMovement = (direction) => {
-    switch (direction) {
-        case 'ArrowLeft':
-            if (mapObject.playerPosition[1] != 0 && !gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1] - 1].isObstacle) {
-                gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
-
-                mapObject.playerPosition[1]--;
-                break;
-            } else {
-                break;
-            }
-
-        case 'ArrowUp':
-            if (mapObject.playerPosition[0] != 0 && !gamePanelsArray[mapObject.playerPosition[0] - 1][mapObject.playerPosition[1]].isObstacle) {
-                gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
-
-                mapObject.playerPosition[0]--;
-                break;
-            } else {
-                break;
-            }
-
-        case 'ArrowRight':
-            if (mapObject.playerPosition[1] != mapObject.size - 1 && !gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1] + 1].isObstacle) {
-                gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
-
-                mapObject.playerPosition[1]++;
-                break;
-            } else {
-                break;
-            }
-
-        case 'ArrowDown':
-            if (mapObject.playerPosition[0] != mapObject.size - 1 && !gamePanelsArray[mapObject.playerPosition[0] + 1][mapObject.playerPosition[1]].isObstacle) {
-                gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'green';
-
-                mapObject.playerPosition[0]++;
-                break;
-            } else {
-                break;
-            }
-
-        default:
-            break;
-    }
-    gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].panelId.style.backgroundColor = 'gold';
-
-    if (gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].isObjective == true) {
-        gamePanelsArray[mapObject.playerPosition[0]][mapObject.playerPosition[1]].isObjective = false;
-        victoryCount--;
-        changeObjective();
-    }
-
-    // Poniższy setTimeout() "opóźnia" (chyba...?) w jakiś sposób wykonanie tego alertu, tak żeby najpierw poszedł DOM
-    // zmieniający kolor kafelka na żółty.
-    // Wiem że działa, ale nie wiem dlaczego.
-
-    setTimeout(() => {
-        if (victoryCount == 0) {
-            document.getElementById('game-container-win').style.opacity = 1;
-        }
-    }, 0);
-}
-
-
-// EVENTS HANDLING
-
-document.addEventListener('keydown', (e) => {
-    playerMovement(e.code);
-});
